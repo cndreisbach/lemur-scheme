bf06bda6d44804508f8673a4adc89ed1ab6d0ecb
Clinton <cnixon@eeek.(none)>
2009-02-04 23:11:11 -0500

    started a Ruby Scheme interpreter

---
+++ ruby/env.rb
@@ -0,0 +1,31 @@
+class Env
+  def initialize(parent=nil, defaults={})
+    @parent = parent
+    @defs = defaults
+  end
+
+  def define(symbol, value)
+    @defs[symbol] = value
+  end
+
+  def defined?(symbol)
+    @defs.has_key?(symbol) or (@parent and @parent.defined?(symbol))
+  end
+
+  def lookup(symbol)
+    begin
+      return (@defs[symbol] || @parent.lookup(symbol))
+    rescue NoMethodError
+      raise "No value for symbol #{symbol}"
+    end
+  end
+
+  def set!(symbol, value)
+    if @defs.has_key?(symbol)
+      @defs[symbol] = value
+    else
+      raise "No definition of #{symbol} to set to #{value}" if @parent.nil?
+      @parent.set!(symbol, value)
+    end
+  end
+end
