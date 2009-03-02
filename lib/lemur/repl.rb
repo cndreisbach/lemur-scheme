23b96c0a179e6285af7dfceb0700a7caffe53070
Clinton R. Nixon <crnixon@gmail.com>
2009-03-01 21:19:28 -0500

    REPL made much better

--- lib/lemur/repl.rb
+++ lib/lemur/repl.rb
@@ -5,14 +5,26 @@ module Lemur
     end
     
     def run
+      puts "; Welcome to the Lemur Scheme REPL!"
+      puts "; Enter your expressions below, and they will be evaluated."
+      puts "; Expression results will appear as comments.\n\n"
+      line = ''
       loop do
-        line = Readline::readline('> ')
+        line += Readline::readline('')
         Readline::HISTORY.push(line)
         begin
-          puts @interpreter.eval(line).to_scm
+          puts "; #{@interpreter.eval(line).to_scm}\n\n"
+        rescue RParsec::ParserException => e
+          if e.to_s =~ /^'\)' expected/
+            line += " "
+            redo
+          else
+            puts "; SYNTAX ERROR: #{e}\n\n"
+          end
         rescue StandardError => e
-          puts "ERROR: #{e}"
+          puts "; ERROR: #{e}\n\n"
         end
+        line = ''
       end
     end     
   end
